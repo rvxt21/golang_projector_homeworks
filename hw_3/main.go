2bd7602b988ec9b74b59c02b5ef6a48918c7ea96
rvxt21 <anastasia1koryagina@gmail.com>
2024-06-08 23:45:18 +0300

    updates on hw 03

--- hw_3/main.go
+++ hw_3/main.go
@@ -21,6 +21,24 @@ type Situation struct {
 	Next3       *Situation
 }
 
+func NewSituation(description, option1, option2, option3 string, next1, next2, next3 *Situation) *Situation {
+	return &Situation{
+		Description: description,
+		Option1:     option1,
+		Next1:       next1,
+		Option2:     option2,
+		Next2:       next2,
+		Option3:     option3,
+		Next3:       next3,
+	}
+}
+
+func NewSituationWithoutOptions(description string) *Situation {
+	return &Situation{
+		Description: description,
+	}
+}
+
 type EndSituation struct {
 	Description string
 }
@@ -29,80 +47,78 @@ func (s Situation) PrintSituationWithOptions() {
 	fmt.Printf("%s.\nВаріант 1: %s\nВаріант 2: %s\nВаріант 3: %s.\nОбери свій шлях!\n", s.Description, s.Option1, s.Option2, s.Option3)
 }
 
-func StartTheGame() {
+func StartTheGame() Character {
 	var name string
 	fmt.Println("Введи своє ім'я, Гравець!")
 	fmt.Scan(&name)
 	character := Character{name, "факел та ніж"}
 	character.PrintCharacterInfo()
+	return character
 }
 
-func Game() {
-	startSituation := Situation{
-		Description: "Ти прокидаєшся в старовинній кімнаті замку. Ти бачиш три виходи: двері ліворуч, двері праворуч та таємний хід прямо.",
-		Option1:     "Зайти в двері ліворуч.",
-		Option2:     "Зайти в двері праворуч",
-		Option3:     "Зайти в таємний хід прямо.",
-	}
+func GenerateStartSituation(ch Character) *Situation {
+	return NewSituation(
+		fmt.Sprintf("Ти прокидаєшся в старовинній кімнаті замку. Твій інвентар: %s. Ти бачиш три виходи: двері ліворуч, двері праворуч та таємний хід прямо", ch.Inventory),
+		"Зайти в двері ліворуч.",
+		"Зайти в двері праворуч",
+		"Зайти в таємний хід прямо.",
+		nil, nil, nil,
+	)
+}
 
-	libraryWay := &Situation{
-		Description: "Ви потряпляєте у бібліотеку. У бібліотеці мудрець пропонує вам три книги: про магію, про історію замку та про монстрів.",
-		Option1:     "Взяти про магію",
-		Option2:     "Взяти книгу про історію замку",
-		Option3:     "Взяти книгу про монстрів",
-	}
+func LibraryWayOption() *Situation {
+	return NewSituation(
+		"Ви потрапляєте у бібліотеку. У бібліотеці мудрець пропонує вам три книги: про магію, про історію замку та про монстрів.",
+		"Взяти про магію",
+		"Взяти книгу про історію замку",
+		"Взяти книгу про монстрів",
+		nil, nil, nil,
+	)
+}
 
-	paintingRoomWay := &Situation{
-		Description: "Ви потрапляєте у залу з картинами. У залі з картинами одна з картин починає рухатися. Ви можете дослідити її, шукати прихований прохід або повернутися назад.",
-		Option1:     "Досліджувати картину.",
-		Option2:     "Шукати прихований прохід.",
-		Option3:     "Повернутися назад.",
-	}
-	dungeonWay := &Situation{
-		Description: "Ви потрапляєте у підземелля. У підземеллі ви зустрічаєте трьох монстрів: дракона, гобліна і привида. Ви повинні вибрати, з ким битися.",
-		Option1:     "Битися з драконом",
-		Option2:     "Битися з гобліном",
-		Option3:     "Битися з привидом",
-	}
+func PaintingRoomOption() *Situation {
+	return NewSituation(
+		"Ви потрапляєте у залу з картинами. У залі з картинами одна з картин починає рухатися. Ви можете дослідити її, шукати прихований прохід або повернутися назад.",
+		"Досліджувати картину.",
+		"Шукати прихований прохід.",
+		"Повернутися назад.",
+		nil, nil, nil,
+	)
+}
 
-	startSituation.Next1 = libraryWay
-	startSituation.Next2 = paintingRoomWay
-	startSituation.Next3 = dungeonWay
-	magicBook := &Situation{
-		Description: "Ви дізнаєтеся закляття, яке допомагає вам вийти з замку. Ви успішно втекли! Кінець гри.",
-	}
-	historyBook := &Situation{
-		Description: "Ви знаходите підказку про прихований вихід і успішно втекли! Кінець гри.",
-	}
-	monsterBook := &Situation{
-		Description: "Ви дізнаєтеся слабкі місця монстрів і використовуєте цю інформацію, щоб втекти з замку! Кінець гри.",
-	}
+func DungeonWayOption() *Situation {
+	return NewSituation(
+		"Ви потрапляєте у підземелля. У підземеллі ви зустрічаєте трьох монстрів: дракона, гобліна і привида. Ви повинні вибрати, з ким битися.",
+		"Битися з драконом",
+		"Битися з гобліном",
+		"Битися з привидом",
+		nil, nil, nil,
+	)
+}
 
-	examinePainting := &Situation{
-		Description: "Ви потрапляєте в секретну кімнату з скарбами. Ви знаходите вихід і успішно тікаєте! Кінець гри.",
-	}
-	hiddenPassage := &Situation{
-		Description: "Ви знаходите таємний тунель, що веде на свободу. Ви успішно втекли! Кінець гри.",
-	}
-	returnToStart := &Situation{
-		Description: "Ви повертаєтеся назад до початку гри.",
-		Option1:     "ліворуч",
-		Option2:     "праворуч",
-		Option3:     "прямо",
-		Next1:       libraryWay,
-		Next2:       paintingRoomWay,
-		Next3:       dungeonWay,
-	}
+func ConnectSituations(startSituation *Situation) {
+	libraryWay := LibraryWayOption()
+	paintingRoomWay := PaintingRoomOption()
+	dungeonWay := DungeonWayOption()
 
-	dragonFight := &Situation{
-		Description: "Ви знайшли спосіб перемогти дракона і втекли з замку! Кінець гри.",
-	}
-	goblinFight := &Situation{
-		Description: "Ви вирішили головоломку і перемогли гобліна! Ви втекли з замку! Кінець гри.",
-	}
-	ghostFight := &Situation{
-		Description: "Ви знайшли свічку і вигнали привида! Ви втекли з замку! Кінець гри.",
-	}
+	magicBook := NewSituationWithoutOptions("Ви дізнаєтеся закляття, яке допомагає вам вийти з замку. Ви успішно втекли! Кінець гри.")
+	historyBook := NewSituationWithoutOptions("Ви знаходите підказку про прихований вихід і успішно втекли! Кінець гри.")
+	monsterBook := NewSituationWithoutOptions("Ви дізнаєтеся слабкі місця монстрів і використовуєте цю інформацію, щоб втекти з замку! Кінець гри.")
+
+	examinePainting := NewSituationWithoutOptions("Ви потрапляєте в секретну кімнату з скарбами. Ви знаходите вихід і успішно тікаєте! Кінець гри.")
+	hiddenPassage := NewSituationWithoutOptions("Ви знаходите таємний тунель, що веде на свободу. Ви успішно втекли! Кінець гри.")
+
+	returnToStart := NewSituation(
+		"Ви повертаєтеся назад до початку гри.",
+		"ліворуч",
+		"праворуч",
+		"прямо",
+		libraryWay, paintingRoomWay, dungeonWay,
+	)
+
+	dragonFight := NewSituationWithoutOptions("Ви знайшли спосіб перемогти дракона і втекли з замку! Кінець гри.")
+	goblinFight := NewSituationWithoutOptions("Ви вирішили головоломку і перемогли гобліна! Ви втекли з замку! Кінець гри.")
+	ghostFight := NewSituationWithoutOptions("Ви знайшли свічку і вигнали привида! Ви втекли з замку! Кінець гри.")
 
 	libraryWay.Next1 = magicBook
 	libraryWay.Next2 = historyBook
@@ -116,9 +132,18 @@ func Game() {
 	dungeonWay.Next2 = goblinFight
 	dungeonWay.Next3 = ghostFight
 
+	startSituation.Next1 = libraryWay
+	startSituation.Next2 = paintingRoomWay
+	startSituation.Next3 = dungeonWay
+}
+
+func Game(ch Character) {
+	startSituation := GenerateStartSituation(ch)
+	ConnectSituations(startSituation)
+
 	currentSituation := startSituation
 
-	for true {
+	for {
 		currentSituation.PrintSituationWithOptions()
 		var userInputChoice string
 		fmt.Scan(&userInputChoice)
@@ -126,15 +151,15 @@ func Game() {
 		switch userInputChoice {
 		case "1":
 			if currentSituation.Next1 != nil {
-				currentSituation = *currentSituation.Next1
+				currentSituation = currentSituation.Next1
 			}
 		case "2":
 			if currentSituation.Next2 != nil {
-				currentSituation = *currentSituation.Next2
+				currentSituation = currentSituation.Next2
 			}
 		case "3":
 			if currentSituation.Next3 != nil {
-				currentSituation = *currentSituation.Next3
+				currentSituation = currentSituation.Next3
 			}
 		default:
 			fmt.Println("Ваш вибір не відомий, оберіть інший!")
@@ -148,6 +173,7 @@ func Game() {
 }
 
 func main() {
-	StartTheGame()
-	Game()
+	character := StartTheGame()
+	fmt.Println(character.Name, "починає гру!")
+	Game(character)
 }
