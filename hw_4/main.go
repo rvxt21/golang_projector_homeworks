00629c7da3c772f806fffe0bebd2f6758a42625a
rvxt21 <anastasia1koryagina@gmail.com>
2024-06-09 23:01:51 +0300

    updates on hw4

--- hw_4/main.go
+++ hw_4/main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"fmt"
+	"math/rand/v2"
+	"slices"
 	"strings"
 )
 
 func main() {
+	fmt.Println("*******************FIRST****************************")
 	fmt.Println("Please input string to find:")
 	var UserInputToFindSlice string
 	fmt.Scan(&UserInputToFindSlice)
 	fmt.Printf("Your word to find: %s.\n", UserInputToFindSlice)
 	CreatedTextByUser := CreateText()
 	FindSliceInText(CreatedTextByUser, UserInputToFindSlice)
+
+	fmt.Println("*******************SECOND****************************")
+	startSlice := CreateSlice()
+	result := MakeElementsUnique(startSlice)
+	fmt.Println(result)
+
 }
 
 func FindSliceInText(Slice []string, WordToFind string) {
@@ -35,7 +44,7 @@ func CreateText() []string {
 	var UserInput string
 	fmt.Println("******************************")
 	fmt.Println("Input words, for new word press Enter. To stop press 'q'")
-	for true {
+	for {
 		fmt.Print(">")
 		fmt.Scan(&UserInput)
 		if UserInput == "q" {
@@ -46,4 +55,57 @@ func CreateText() []string {
 	fmt.Println("******************************")
 	fmt.Println(UserCreatedText)
 	return UserCreatedText
+}
+
+type IDs struct {
+	ID int
+}
+
+func MakeElementsUnique(slice []IDs) []IDs {
+	unique := UniqueValuesSlice(slice)
+	sorted := SortSlice(unique)
+
+	return sorted
+}
+
+func CreateSlice() []IDs {
+	slice := []IDs{}
+
+	for i := 0; i < 20; i++ {
+		new_id := IDs{ID: rand.IntN(50)}
+		slice = append(slice, new_id)
+	}
+
+	fmt.Println(slice)
+	return slice
+}
+
+func UniqueValuesSlice(slice []IDs) []IDs {
+	var uniqueElemsSlice []IDs
+	for _, elem := range slice {
+		if slices.Contains(uniqueElemsSlice, elem) {
+			continue
+		} else {
+			uniqueElemsSlice = append(uniqueElemsSlice, elem)
+		}
+	}
+	return uniqueElemsSlice
+}
+
+func SortSlice(slice []IDs) []IDs {
+	len := len(slice)
+	for i := 0; i < len-1; i++ {
+		swapped := false
+		for j := 0; j < len-i-1; j++ {
+			if slice[j].ID > slice[j+1].ID {
+				slice[j], slice[j+1] = slice[j+1], slice[j]
+				swapped = true
+			}
+
+		}
+		if !swapped {
+			break
+		}
+	}
+	return slice
 }
