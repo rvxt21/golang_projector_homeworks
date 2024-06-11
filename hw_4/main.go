e80560dbbaef988bea31de1f7ad8542299b22a6d
rvxt21 <anastasia1koryagina@gmail.com>
2024-06-11 13:00:57 +0300

    used sort.Slice function in hw_4

--- hw_4/main.go
+++ hw_4/main.go
@@ -4,17 +4,18 @@ import (
 	"fmt"
 	"math/rand/v2"
 	"slices"
+	"sort"
 	"strings"
 )
 
 func main() {
 	fmt.Println("*******************FIRST****************************")
 	fmt.Println("Please input string to find:")
-	var UserInputToFindSlice string
-	fmt.Scan(&UserInputToFindSlice)
-	fmt.Printf("Your word to find: %s.\n", UserInputToFindSlice)
-	CreatedTextByUser := CreateText()
-	FindSliceInText(CreatedTextByUser, UserInputToFindSlice)
+	var userInputToFindSlice string
+	fmt.Scan(&userInputToFindSlice)
+	fmt.Printf("Your word to find: %s.\n", userInputToFindSlice)
+	createdTextByUser := CreateText()
+	FindSliceInText(createdTextByUser, userInputToFindSlice)
 
 	fmt.Println("*******************SECOND****************************")
 	startSlice := CreateSlice()
@@ -23,13 +24,13 @@ func main() {
 
 }
 
-func FindSliceInText(Slice []string, WordToFind string) {
+func FindSliceInText(Slice []string, wordToFind string) {
 	fmt.Println("******************************")
 	fmt.Println("Results:")
 	found := false
-	WordToFind = strings.ToLower(WordToFind)
+	wordToFind = strings.ToLower(wordToFind)
 	for i, val := range Slice {
-		if strings.Contains(strings.ToLower(val), WordToFind) {
+		if strings.Contains(strings.ToLower(val), wordToFind) {
 			fmt.Printf("Line %d: %s\n", i+1, val)
 			found = true
 		}
@@ -40,21 +41,21 @@ func FindSliceInText(Slice []string, WordToFind string) {
 }
 
 func CreateText() []string {
-	var UserCreatedText []string
-	var UserInput string
+	var userCreatedText []string
+	var userInput string
 	fmt.Println("******************************")
 	fmt.Println("Input words, for new word press Enter. To stop press 'q'")
 	for {
 		fmt.Print(">")
-		fmt.Scan(&UserInput)
-		if UserInput == "q" {
+		fmt.Scan(&userInput)
+		if userInput == "q" {
 			break
 		}
-		UserCreatedText = append(UserCreatedText, UserInput)
+		userCreatedText = append(userCreatedText, userInput)
 	}
 	fmt.Println("******************************")
-	fmt.Println(UserCreatedText)
-	return UserCreatedText
+	fmt.Println(userCreatedText)
+	return userCreatedText
 }
 
 type IDs struct {
@@ -63,9 +64,9 @@ type IDs struct {
 
 func MakeElementsUnique(slice []IDs) []IDs {
 	unique := UniqueValuesSlice(slice)
-	sorted := SortSlice(unique)
+	sort.Slice(unique, func(i, j int) bool { return unique[i].ID < unique[j].ID })
 
-	return sorted
+	return unique
 }
 
 func CreateSlice() []IDs {
@@ -92,20 +93,20 @@ func UniqueValuesSlice(slice []IDs) []IDs {
 	return uniqueElemsSlice
 }
 
-func SortSlice(slice []IDs) []IDs {
-	len := len(slice)
-	for i := 0; i < len-1; i++ {
-		swapped := false
-		for j := 0; j < len-i-1; j++ {
-			if slice[j].ID > slice[j+1].ID {
-				slice[j], slice[j+1] = slice[j+1], slice[j]
-				swapped = true
-			}
+// func SortSlice(slice []IDs) []IDs {
+// 	len := len(slice)
+// 	for i := 0; i < len-1; i++ {
+// 		swapped := false
+// 		for j := 0; j < len-i-1; j++ {
+// 			if slice[j].ID > slice[j+1].ID {
+// 				slice[j], slice[j+1] = slice[j+1], slice[j]
+// 				swapped = true
+// 			}
 
-		}
-		if !swapped {
-			break
-		}
-	}
-	return slice
-}
+// 		}
+// 		if !swapped {
+// 			break
+// 		}
+// 	}
+// 	return slice
+// }
